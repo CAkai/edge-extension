754c9d24bf642dec5868729f88229b8cfc6c88b0
Arvin Yang <sugky7302@gmail.com>
2024-10-21 13:09:54 +0800

    fix: 修正 background.js 會報錯的問題

--- translate/edge/background.js
+++ translate/edge/background.js
@@ -1,19 +1,19 @@
 const BING_ORIGIN = 'https://www.bing.com';
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: 'openSidePanel',
-    title: 'Open sidebar',
-    contexts: ['all']
-  });
+  // chrome.contextMenus.create({
+  //   id: 'openSidePanel',
+  //   title: 'Open sidebar',
+  //   contexts: ['all']
+  // });
 });
 
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === 'openSidePanel') {
-    // Open the sidebar in all the tabs of the current window.
-    chrome.sidePanel.open({ windowId: tab.windowId });
-  }
-});
+// chrome.contextMenus.onClicked.addListener((info, tab) => {
+//   if (info.menuItemId === 'openSidePanel') {
+//     // Open the sidebar in all the tabs of the current window.
+//     chrome.sidePanel.open({ windowId: tab.windowId });
+//   }
+// });
 
 chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
   if (!tab.url) return;
