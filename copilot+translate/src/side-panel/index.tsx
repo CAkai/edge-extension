bf7131cdcfb8a86e34f8cd1573c2acf20e5e08f6
Arvin Yang <sugky7302@gmail.com>
2024-10-25 13:41:54 +0800

    chg: 將畫面元素置中

--- copilot+translate/src/side-panel/index.tsx
+++ copilot+translate/src/side-panel/index.tsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import Login from './login';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Login from "./login";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Login />
-  </StrictMode>,
+    <div className="h-dvh min-w-[360px] p-1">
+      <div className="h-full flex items-center justify-center">
+        <Login />
+      </div>
+    </div>
+  </StrictMode>
 );
