ae18ba62302bdaed03396e4d56296f74f7c9680b
Arvin Yang <sugky7302@gmail.com>
2024-11-07 15:10:36 +0800

    feat: 加入 Model 選單

--- copilot+translate/src/side-panel/chat.tsx
+++ copilot+translate/src/side-panel/chat.tsx
@@ -1,29 +1,81 @@
 import SendIcon from './public/send.svg?react';
+import LlmIcon from './public/model.svg?react';
 import './chat.css';
 import { ReactElement, useEffect, useState } from 'react';
 import { loadLlmModels } from '../store/llm.store';
 import { useAppDispatch, useAppSelector } from '../store';
 
 function MessageBox() {
+    return (
+        <div className="h-5/6 border border-black">
+            <h1>Message Box</h1>
+        </div>
+    );
+}
+
+function ModelButton() {
     const dispatch = useAppDispatch();
     const llm = useAppSelector(state => state.llm);
     const user = useAppSelector(state => state.user);
     const [models, setModels] = useState<ReactElement[]>([]);
+    const [isExpanded, setIsExpanded] = useState(false);
 
     useEffect(() => {
-        console.log(user);
         dispatch(loadLlmModels(user.webui_info.token));
     }, [dispatch, user]);
 
     useEffect(() => {
-        setModels(llm?.map(m => <p key={m.id}>{m.name}</p>) ?? []);
+        setModels(
+            llm?.map(m => (
+                <a
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700"
+                    role="menuitem"
+                    tabIndex={-1}
+                    id="menu-item-0">
+                    {m.name}
+                </a>
+            )) ?? [],
+        );
     }, [llm]);
 
     return (
-        <div className="h-5/6 border border-black">
-            <h1>Message Box</h1>
-            <p>{import.meta.env.VITE_ICLOUD_URL}</p>
-            {models}
+        <div className="relative inline-block text-left">
+            <div>
+                <button
+                    type="button"
+                    className="inline-flex w-full justify-center gap-x-0.5 rounded-md bg-white p-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                    id="menu-button"
+                    aria-expanded={isExpanded}
+                    aria-haspopup="menu"
+                    onClick={() => setIsExpanded(!isExpanded)}>
+                    <LlmIcon style={{ width: '20px', height: '20px' }} />
+                    <svg
+                        className={`-mr-1 h-5 w-5 text-gray-400 transform transition-transform duration-300 ${
+                            isExpanded ? '-rotate-180' : ''
+                        }`}
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                        data-slot="icon">
+                        <path
+                            fill-rule="evenodd"
+                            d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
+                            clip-rule="evenodd"
+                        />
+                    </svg>
+                </button>
+            </div>
+            <div
+                className={`absolute left-0 bottom-[2.5rem] z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none scale-animation ${isExpanded ? 'scale-on' : 'scale-off'}`}
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="menu-button"
+                tabIndex={-1}>
+                <div className="py-1" role="none">
+                    {models}
+                </div>
+            </div>
         </div>
     );
 }
@@ -52,7 +104,9 @@ const MessageInput = () => {
 
     return (
         <div className="h-1/6 flex flex-col justify-end">
-            <div>Message Tools</div>
+            <div>
+                <ModelButton />
+            </div>
             <div className="rounded-lg border border-black w-full h-28 relative">
                 <textarea
                     // 這邊加 bg-transparent 是因為 textarea 會有一個預設的背景色，這樣會切到 border 的四個角
