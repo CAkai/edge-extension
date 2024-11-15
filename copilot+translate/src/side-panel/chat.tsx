c27932ad7d306afd93d3add542a1772b1f1b08ed
Arvin Yang <sugky7302@gmail.com>
2024-11-15 14:47:51 +0800

    feat: 讀取 Open WebUI 的歷史訊息列表

--- copilot+translate/src/side-panel/chat.tsx
+++ copilot+translate/src/side-panel/chat.tsx
@@ -1,11 +1,13 @@
 import SendIcon from './public/send.svg?react';
 import LlmIcon from './public/model.svg?react';
+import HistoryIcon from './public/history.svg?react';
 import './chat.css';
 import React, { createRef, ReactElement, useEffect, useState } from 'react';
 import { loadLlmModels } from '../store/llm.store';
 import { RootState, useAppDispatch, useAppSelector } from '../store';
 import { useStore } from 'react-redux';
 import { GenerateRequest } from '../store/message.store';
+import { getChatList } from '../store/chat.store';
 
 function MessageBox() {
     const messages = useAppSelector(state => state.message);
@@ -22,9 +24,8 @@ function MessageBox() {
                 <div key={i} className={`p-1 flex flex-col ${m.role === 'user' ? 'items-end' : 'items-start'}`}>
                     {m.role === 'user' ? false : <p>{m.role}</p>}
                     <div
-                        className={`p-2 rounded-lg w-fit max-w-[95%] text-base ${
-                            m.role === 'user' ? 'bg-gray-200' : 'bg-slate-50'
-                        }`}>
+                        className={`p-2 rounded-lg w-fit max-w-[95%] text-base ${m.role === 'user' ? 'bg-gray-200' : 'bg-slate-50'
+                            }`}>
                         {m.content}
                     </div>
                 </div>
@@ -42,6 +43,94 @@ function MessageBox() {
     );
 }
 
+// 歷史訊息按鈕，會同步 Open WebUI
+function HistoryButton() {
+    const buttonRef = createRef<HTMLDivElement>();  // 檢查是否點擊在按鈕上
+    const dispatch = useAppDispatch();
+    const hist = useAppSelector(state => state.chat);
+    const user = useAppSelector(state => state.user);
+    const [models, setModels] = useState<ReactElement[]>([]);
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    // 載入歷史訊息
+    useEffect(() => {
+        dispatch(getChatList(user.webui_info.token));
+    }, [dispatch, user]);
+
+    // 更新歷史訊息列表
+    useEffect(() => {
+        const selected = hist?.selected ?? '';
+        setModels(
+            hist?.chats.map((m, i) => (
+                <button
+                    className={`w-full text-left block px-4 py-2 text-sm text-gray-700 ${selected === m.id ? 'bg-gray-200' : ''
+                        } hover:bg-gray-100`}
+                    role="menuitem"
+                    tabIndex={-1}
+                    onClick={() => {
+                        dispatch({ type: 'chat/select', payload: m.id });
+                        setIsExpanded(false);
+                    }}
+                    id={`chat-hist-${i}`}>
+                    {m.title}
+                </button>
+            )) ?? [],
+        );
+    }, [hist, dispatch]);
+
+    // 監聽點擊事件，如果點擊的地方不是在 button 上，就關閉選單
+    useEffect(() => {
+        const handleClickOutside = (e: MouseEvent) => {
+            // e.target 要轉型成 Node，否則會報錯「EventTarget | null is not assignable to type Node」
+            if (buttonRef.current && !buttonRef.current.contains(e.target as Node)) {
+                setIsExpanded(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => document.removeEventListener('mousedown', handleClickOutside);
+    }, [buttonRef]);
+
+    return (
+        <div ref={buttonRef} className="relative inline-block text-left">
+            <div>
+                <button
+                    type="button"
+                    className="inline-flex w-full justify-center gap-x-0.5 rounded-md p-1 bg-white text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                    id="menu-button"
+                    aria-expanded={isExpanded}
+                    aria-haspopup="menu"
+                    onClick={() => setIsExpanded(!isExpanded)}>
+                    <HistoryIcon style={{ width: '20px', height: '20px' }} />
+                    <svg
+                        className={`-mr-1 h-5 w-5 text-gray-400 transform transition-transform duration-300 ${isExpanded ? '-rotate-180' : ''
+                            }`}
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                        data-slot="icon">
+                        <path
+                            fill-rule="evenodd"
+                            d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
+                            clip-rule="evenodd"
+                        />
+                    </svg>
+                </button>
+            </div>
+            <div
+                className={`absolute right-0 bottom-[2.5rem] z-10 mt-2 w-56 origin-bottom-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none scale-animation ${isExpanded ? 'scale-on' : 'scale-off'
+                    }`}
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="menu-button"
+                tabIndex={-1}>
+                <div role="none">{models}</div>
+            </div>
+        </div>
+    );
+}
+
+// 模型選擇按鈕
 function ModelButton() {
     const buttonRef = createRef<HTMLDivElement>();
     const dispatch = useAppDispatch();
@@ -61,9 +150,8 @@ function ModelButton() {
         setModels(
             llm?.models.map((m, i) => (
                 <button
-                    className={`w-full text-left block px-4 py-2 text-sm text-gray-700 ${
-                        selectedModel === m.id ? 'bg-gray-200' : ''
-                    } hover:bg-gray-100`}
+                    className={`w-full text-left block px-4 py-2 text-sm text-gray-700 ${selectedModel === m.id ? 'bg-gray-200' : ''
+                        } hover:bg-gray-100`}
                     role="menuitem"
                     tabIndex={-1}
                     onClick={() => {
@@ -102,9 +190,8 @@ function ModelButton() {
                     onClick={() => setIsExpanded(!isExpanded)}>
                     <LlmIcon style={{ width: '20px', height: '20px' }} />
                     <svg
-                        className={`-mr-1 h-5 w-5 text-gray-400 transform transition-transform duration-300 ${
-                            isExpanded ? '-rotate-180' : ''
-                        }`}
+                        className={`-mr-1 h-5 w-5 text-gray-400 transform transition-transform duration-300 ${isExpanded ? '-rotate-180' : ''
+                            }`}
                         viewBox="0 0 20 20"
                         fill="currentColor"
                         aria-hidden="true"
@@ -118,9 +205,8 @@ function ModelButton() {
                 </button>
             </div>
             <div
-                className={`absolute left-0 bottom-[2.5rem] z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none scale-animation ${
-                    isExpanded ? 'scale-on' : 'scale-off'
-                }`}
+                className={`absolute left-0 bottom-[2.5rem] z-10 mt-2 w-56 origin-bottom-left rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none scale-animation ${isExpanded ? 'scale-on' : 'scale-off'
+                    }`}
                 role="menu"
                 aria-orientation="vertical"
                 aria-labelledby="menu-button"
@@ -242,9 +328,12 @@ const MessageInput = () => {
 
     return (
         <div className="flex flex-col justify-end">
-            <div className="flex gap-1 items-center">
-                <ModelButton />
-                <p>{selectedModel}</p>
+            <div className="flex justify-between mb-1">
+                <div className="flex gap-1 items-center">
+                    <ModelButton />
+                    <p>{selectedModel}</p>
+                </div>
+                <HistoryButton />
             </div>
             <div className="rounded-lg border border-black w-full h-28 relative">
                 <textarea
--- copilot+translate/src/store/chat.store.ts
+++ copilot+translate/src/store/chat.store.ts
@@ -7,23 +7,39 @@ export type Chat = {
     created_at: number;
 }
 
-const initialState: Chat[] = [];
+const initialState: {selected: string; chats: Chat[]} = {
+    selected: "",
+    chats: [],
+}
 
 export const getChatList = createAsyncThunk(
     'chat/getList',
-    async (token: string) => {
+    async (token: string, {getState}) => {
+        const state = getState() as { chat: { selected: string; models: Chat[] } };
+
         if (!token) return;
-        
+        console.log("webui token", token);
         // 到 iCloud 取得使用者資料
-        return await fetch(import.meta.env.VITE_OPEN_WEBUI_URL + "api/models", {
+        return await fetch(import.meta.env.VITE_OPEN_WEBUI_URL + "api/v1/chats/list", {
             method: "GET",
             headers: {
                 "Authorization": `Bearer ${token}`,
             },
         })
-            .then((res) => res.json())
+            .then((res) => res.json() as Promise<Chat[]>)
+            .then((data) => {
+                if (data.length > 0) {
+                    return {
+                        selected: data[0].id,
+                        models: data,
+                    }
+                } else {
+                    return state.chat;
+                }
+            })
             .catch((err) => {
                 console.error("error", err);
+                return state.chat;
             });
     }
 );
@@ -31,11 +47,20 @@ export const getChatList = createAsyncThunk(
 export const chatSlice = createSlice({
     name: 'chat',
     initialState,
-    reducers: {},
+    reducers: {
+        select: (state, action) => {
+            state.selected = action.payload;
+        }
+    },
     extraReducers: (builder) => {
-        builder.addCase(getChatList.fulfilled, (_, {payload}) => {
-            if (!payload) return [];
-            return Array.from(payload);
+        builder.addCase(getChatList.fulfilled, (state, {payload}) => {
+            console.log(payload);
+            if (!payload) return {...state}
+            return {
+                ...state,
+                selected: payload.selected,
+                chats: payload.models
+            }
         });
     }
 })
