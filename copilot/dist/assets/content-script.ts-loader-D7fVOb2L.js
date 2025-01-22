5c1d0d74cdb27bb90ddb7401283deff450e9bacf
Arvin Yang <sugky7302@gmail.com>
2025-01-22 12:20:58 +0800

    fix: 修正驗證 Open WebUI Token 的網址錯誤，導致無法登入的問題。

--- copilot/dist/assets/content-script.ts-loader-D7fVOb2L.js
+++ copilot/dist/assets/content-script.ts-loader-D7fVOb2L.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/content-script.ts-_HHbKvhy.js")
+      chrome.runtime.getURL("assets/content-script.ts-o0BjXaSN.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
